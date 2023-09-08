9aadf3575ee3304e32679cdf4592207725f8cde5
Bruno Paulin <bruno.p-@hotmail.com>
2023-09-08 20:05:36 -0300

    finish Intent lesson

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Button btnSend;
     private EditText edtMain;
+    private TextView txtMainReply;
     public static final String EXTRA_MESSAGE = "com.example.myapplication.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnSend = findViewById(R.id.button_main);
         edtMain = findViewById(R.id.editText_main);
+        txtMainReply = findViewById(R.id.text_message_reply);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +36,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "message: "+message);
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
+                startActivityForResult(intent, TEXT_REQUEST);
             }
         });
+
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(TAG, "entrei no Result");
+        Log.d(TAG, "requestCode: "+requestCode);
+        Log.d(TAG, "resultCode: "+resultCode);
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                txtMainReply.setText(reply);
+                txtMainReply.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -5,23 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private final static String TAG = SecondActivity.class.getSimpleName();
-    private TextView txtHeader, txtMessage;
+    public static final String EXTRA_REPLY = TAG+".extra.Reply";
+    private TextView txtMessage;
+    private EditText edtMessageReply;
+    private Button btnReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        txtHeader = findViewById(R.id.text_header);
-        txtMessage = findViewById(R.id.text_message);
+        init();
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         Log.d(TAG, "message recebida: "+message);
         txtMessage.setText(message);
 
+        btnReply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String reply = edtMessageReply.getText().toString();
+                Intent replyIntent = new Intent();
+                replyIntent.putExtra(EXTRA_REPLY, reply);
+                setResult(RESULT_OK, replyIntent);
+                finish();
+            }
+        });
+
+    }
+
+    private void init() {
+        txtMessage = findViewById(R.id.text_message);
+        edtMessageReply = findViewById(R.id.editText_second);
+        btnReply = findViewById(R.id.button_second);
     }
 }
